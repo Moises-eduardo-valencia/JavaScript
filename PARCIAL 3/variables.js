8b5f41e8120eee4a5f2f2696c081446cfce75f99
Moises-eduardo-valencia <moisesvalencia835@gmail.com>
2025-03-25 08:56:07 -0600

    Add files via upload

---
+++ PARCIAL 3/variables.js
@@ -0,0 +1,14 @@
+//cd "C:\Users\HP\OneDrive\Documents\5to Cuatrimestre\PROGRAMACIÓN WEB\PARCIAL 3" para poder checar los resultados
+var nombre="Moisés";
+var Nombre=5
+console.log(nombre);
+console.log(Nombre);
+/*se utiliza para declarar variables que pueden ser reasignadas a lo largo delEn JavaScript, 
+let se utiliza para declarar variables que pueden ser reasignadas a lo largo del tiempo. 
+A diferencia de var, lettiene un alcance de bloque, lo que significa que solo es accesible 
+dentro del bloque o la función en la que fue declarada.
+Por ejemplo:*/
+let apellido = "Pérez";
+apellido = "García";  /* Se puede reasignar
+dentro del bloque{let apellido=""} se debe de imprimir dentro no afuera
+El uso de letayuda a evitar errores relacionados con el alcance y permite una mejor gestión de las variables./*
